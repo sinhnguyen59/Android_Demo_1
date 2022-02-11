413e56fedc6a99fd75bd73b3d02c526831880854
Sinh Nguyen <snguyen59@aceproject.space>
2022-02-11 11:20:56 -0600

    This is Android snackbar

---
+++ app/src/main/java/com/example/demo_1/MainActivity.java
@@ -0,0 +1,46 @@
+// documents: https://material.io/components/snackbars
+// Tutorial video 1: https://www.youtube.com/watch?v=IRws-Y-LDtM
+// Tutorial video 2: https://www.youtube.com/watch?v=1bBwJDCww3g&t=310s
+// Dependency implementation: https://material.io/develop/android/docs/getting-started
+// Dependency latest version: https://maven.google.com/web/index.html#com.google.android.material:material
+
+package com.example.demo_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnSnackbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSnackbar = findViewById(R.id.btnSnackbar);
+
+        btnSnackbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar snackbar = Snackbar.make(v, "This is the snackbar", Snackbar.LENGTH_LONG);
+//                snackbar.setDuration(5000);
+                snackbar.setAction("UNDO", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Snackbar undoSnackbar = Snackbar.make(view,"Undo successful", Snackbar.LENGTH_SHORT);
+                        undoSnackbar.show();
+                    }
+                });
+//                snackbar.setTextColor(Color.GREEN);
+                snackbar.show();
+            }
+        });
+    }
+}
